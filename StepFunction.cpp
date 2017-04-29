8646b3fece8d52973228c74cbf26f175a0325dd3
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-04-29 16:35:14 +0900

    calcメソッドの実装

--- StepFunction.cpp
+++ StepFunction.cpp
@@ -1,1 +1,19 @@
+StepFunction::StepFunction()
+{
+}
 
+StepFunction::~StepFunction()
+{
+}
+
+double StepFunction::calc( double x )
+{
+    if( x > 0.0 )
+    {
+        return 1.0;
+    }
+    else
+    {
+        return 0.0;
+    }
+}
