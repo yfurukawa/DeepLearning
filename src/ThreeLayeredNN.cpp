13901319b176b8ba6616a802f14c63abea637dfa
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-05-21 22:24:24 +0900

    インスタンス名のリファクタリング

--- src/ThreeLayeredNN.cpp
+++ src/ThreeLayeredNN.cpp
@@ -24,18 +24,18 @@ void ThreeLayeredNN::initialize() {
 
 void ThreeLayeredNN::forward() {
 	cv::Mat a1, z1, a2, z2, a3, y;
-	ActivationFunction l1;
-	OutputLayer ol;
+	ActivationFunction activeFunction;
+	OutputLayer outputLayer;
 
 	cv::Mat x = (cv::Mat_<float>(1, 2) << 1.0, 0.5);
 
 	a1 = x * network.at("W1") + network.at("b1");
-	z1 = l1.sigmoid(a1);
+	z1 = activeFunction.sigmoid(a1);
 	a2 = z1 * network.at("W2") + network.at("b2");
-	z2 = l1.sigmoid(a2);
+	z2 = activeFunction.sigmoid(a2);
 	a3 = z2 * network.at("W3") + network.at("b3");
 
-	ol.identity(&a3);
+	outputLayer.identity(&a3);
 
 	std::cout << a3 << std::endl;
 }
