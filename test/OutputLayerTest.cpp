ee3474823c9c16cef4b244c2fc46230b6f05843f
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-05-21 18:24:01 +0900

    テストを実装した

--- test/OutputLayerTest.cpp
+++ test/OutputLayerTest.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/core.hpp>
 #include "OutputLayerTest.h"
 
 OutputLayerTest::OutputLayerTest() : sut(NULL){
@@ -17,19 +18,40 @@ void OutputLayerTest::TearDown() {
 }
 
 TEST_F (OutputLayerTest, testSoftmax) {
-	cv::Mat srcMatrix = (cv::Mat_<float>(4,1) << 1.0, 2.0, 3.0, 4.0);
+	cv::Mat srcMatrix = (cv::Mat_<float>(4, 1) << 1.0, 2.0, 3.0, 4.0);
+	cv::Mat expectMatrix = (cv::Mat_<float>(4, 1) << 0.0320586, 0.0871443, 0.23688281, 0.64391428);
+
 	sut->softmax(&srcMatrix);
-	std::cout << srcMatrix << std::endl;
+
+	for(int row = 0; row < (srcMatrix).rows; ++row) {
+	    for(int column = 0; column < (srcMatrix).cols; ++column) {
+	        EXPECT_FLOAT_EQ(expectMatrix.at<float>(row, column), srcMatrix.at<float>(row, column));
+	    }
+	}
 }
 
 TEST_F (OutputLayerTest, testSoftmax_general) {
-	cv::Mat srcMatrix = (cv::Mat_<float>(3,1) << 0.3, 2.9, 4.0);
+	cv::Mat srcMatrix = (cv::Mat_<float>(3, 1) << 0.3, 2.9, 4.0);
+	cv::Mat expectMatrix = (cv::Mat_<float>(3, 1) << 0.018211273, 0.24519183, 0.73659688);
+
 	sut->softmax(&srcMatrix);
-	std::cout << srcMatrix << std::endl;
+
+	for(int row = 0; row < (srcMatrix).rows; ++row) {
+	    for(int column = 0; column < (srcMatrix).cols; ++column) {
+	        EXPECT_FLOAT_EQ(expectMatrix.at<float>(row, column), srcMatrix.at<float>(row, column));
+	    }
+	}
 }
 
 TEST_F (OutputLayerTest, testSoftmax_bigNumber) {
 	cv::Mat srcMatrix = (cv::Mat_<float>(3,1) << 1010, 1000, 990);
+	cv::Mat expectMatrix = (cv::Mat_<float>(3, 1) << 0.99995458, 4.5397868e-05, 2.0610602e-09);
+
 	sut->softmax(&srcMatrix);
-	std::cout << srcMatrix << std::endl;
+
+	for(int row = 0; row < (srcMatrix).rows; ++row) {
+	    for(int column = 0; column < (srcMatrix).cols; ++column) {
+	        EXPECT_FLOAT_EQ(expectMatrix.at<float>(row, column), srcMatrix.at<float>(row, column));
+	    }
+	}
 }
