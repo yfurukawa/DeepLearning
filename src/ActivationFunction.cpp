24ad756fc7fd1deb93da9c3e85771a30a6390ff8
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-05-27 11:38:55 +0900

    クラスおよび関数の説明コメントを追加

--- src/ActivationFunction.cpp
+++ src/ActivationFunction.cpp
@@ -1,22 +1,50 @@
-/*
- * Copyright
- *
-*/
+/*!------------------------------------------------
+@file       Hogeh.h
+@brief
+@attention  なし
+--------------------------------------------------*/
+
 #include <iostream>
 #include "./ActivationFunction.h"
 
+/*!------------------------------------------------
+@brief      デフォルトコンストラクタ
+@note       クラスを構築する
+@attention  なし
+--------------------------------------------------*/
 ActivationFunction::ActivationFunction() {
 }
 
+/*!------------------------------------------------
+@brief      デフォルトデストラクタ
+@note       クラスを破棄する
+@attention  なし
+--------------------------------------------------*/
 ActivationFunction::~ActivationFunction() {
 }
 
+/*!------------------------------------------------
+@brief      シグモイド関数
+@note       引数で与えられた行列の各要素に対し、<br>
+              シグモイド関数を適用する
+@param[in]  matrix  cv::Mat型の行列  [-] (-)
+@return     引数と同じcv::Mat型の行列に格納された計算結果
+@attention  マルチスレッドでの動作は未確認
+--------------------------------------------------*/
 cv::Mat ActivationFunction::sigmoid(const cv::Mat& matrix) {
 	cv::Mat tmpMatrix;
 	cv::exp(-1 * matrix, tmpMatrix);
 	return 1/(1 + tmpMatrix);
 }
 
+/*!------------------------------------------------
+@brief      ステップ関数
+@note       引数で与えられた行列の各要素に対し、<br>
+              ステップ関数を適用する
+@param[in]  matrix  cv::Mat型の行列  [-] (-)
+@return     引数と同じcv::Mat型の行列に格納された計算結果
+@attention  マルチスレッドでの動作は未確認
+--------------------------------------------------*/
 cv::Mat ActivationFunction::step(const cv::Mat& matrix) {
 	cv::Mat tmpMatrix = matrix.clone();
 	for(int row = 0; row < matrix.rows; ++row) {
--- src/ActivationFunction.h
+++ src/ActivationFunction.h
@@ -1,21 +1,39 @@
-/**
- * ActivationFunction.h<br>
- * Copyright
- *
- */
+/*!------------------------------------------------
+@file       ActivationFunction.h
+@brief      活性化関数を提供する
+@attention  なし
+--------------------------------------------------*/
 
 #ifndef ACTIVATIONFUNCTION_H_
 #define ACTIVATIONFUNCTION_H_
 
+// インクルードファイル ================================
 #include <opencv2/core.hpp>
 
+// クラスの前方宣言 ====================================
+
+// マクロの宣言 ========================================
+
+// 型の宣言 ============================================
+
+// クラス定義 ==========================================
+/*!--------------------------------------------------
+@class      ActivationFunction.h
+@brief      活性化関数を提供する
+@note       活性化関数（ステップ関数、シグモイド関数）を<br>
+              提供する。<br>
+@attention  マルチスレッドでの動作は未確認
+@see        なし
+--------------------------------------------------*/
 class ActivationFunction {
  public:
-  //! Constractor
+  //! Constructor
   ActivationFunction();
   //! Destructor
   virtual ~ActivationFunction();
+  //! シグモイド関数
   cv::Mat sigmoid(const cv::Mat& matrix);
+  //! ステップ関数
   cv::Mat step(const cv::Mat& matrix);
 
  protected:
