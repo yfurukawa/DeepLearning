8844649d7b8adf3a6986b9a40774cf3f43909331
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-05-27 21:28:32 +0900

    クラスおよび関数の説明コメントを追加

--- src/MnistReaderCore.cpp
+++ src/MnistReaderCore.cpp
@@ -1,33 +1,60 @@
-/*
- * Copyright
- *
-*/
+/*!------------------------------------------------
+@file       MnistReaderCore.h
+@brief      MNISTデータの読み込みで共通する関数を提供する
+@attention  なし
+--------------------------------------------------*/
+
 #include "./MnistReaderCore.h"
 
+/*!------------------------------------------------
+@brief      デフォルトコンストラクタ
+@note       クラスを構築する
+@attention  なし
+--------------------------------------------------*/
 MnistReaderCore::MnistReaderCore() {
 }
 
+/*!------------------------------------------------
+@brief      デフォルトデストラクタ
+@note       クラスを破棄する
+@attention  なし
+--------------------------------------------------*/
 MnistReaderCore::~MnistReaderCore() {
 }
 
-int MnistReaderCore::reverseInt (int i) {
-    unsigned char ch1, ch2, ch3, ch4;
+/*!------------------------------------------------
+@brief      エンディアン変換
+@note       MNISTデータは値がビックエンディアンで格納されているので<br>
+            Intelプロセッサで処理する場合は、リトルエンディアンに<br>
+              変換する必要がある
+@param[in]  data  変換対象のデータ  [-] (-)
+@return     変換された値
+@attention  なし
+--------------------------------------------------*/
+int MnistReaderCore::reverseInt (int data) {
+    unsigned char ch1(0x00), ch2(0x00), ch3(0x00), ch4(0x00);
 
-    ch1 = i & 255;
-    ch2 = (i >> 8) & 255;
-    ch3 = (i >> 16) & 255;
-    ch4 = (i >> 24) & 255;
+    ch1 = data & 255;
+    ch2 = (data >> 8) & 255;
+    ch3 = (data >> 16) & 255;
+    ch4 = (data >> 24) & 255;
 
     return((int) ch1 << 24) + ((int)ch2 << 16) + ((int)ch3 << 8) + ch4;
 }
 
+/*!------------------------------------------------
+@brief      MNISTラベルデータ読み込み
+@note       引数で指定されたファイルからMNISTのラベルデータを<br>
+              読み込む
+@param[in]  filename  MNISTラベルデータファイル  [-] (-)
+@return     MNISTラベルデータが格納されたコンテナ
+@attention  ファイルが存在しない等のエラー処理は未実装
+--------------------------------------------------*/
 std::vector<double> MnistReaderCore::readMnistLabel(const std::string& filename) {
 	std::ifstream file(filename.c_str(), std::ios::binary);
-	int magic_number = 0;
-	int number_of_images = 0;
-	int n_rows = 0;
-	int n_cols = 0;
-	std::vector<double> vecl(number_of_images);
+	int magic_number(0);
+	int number_of_images(0);
+	std::vector<double> vecl;
 
 	if (file.is_open()) {
 		file.read((char*) &magic_number, sizeof(magic_number));
@@ -36,10 +63,9 @@ std::vector<double> MnistReaderCore::readMnistLabel(const std::string& filename)
 		number_of_images = reverseInt(number_of_images);
 
 		for(int i = 0; i < number_of_images; ++i) {
-			unsigned char temp = 0;
+			unsigned char temp(0);
 			file.read((char*) &temp, sizeof(temp));
-			//vecl[i]= (double)temp;
-			vecl.push_back((double)temp);
+			vecl.push_back(static_cast<double>(temp));
 		}
     }
 	return vecl;
--- src/MnistReaderCore.h
+++ src/MnistReaderCore.h
@@ -1,29 +1,46 @@
-/**
- * MnistReaderCore.h<br>
- * Copyright
- *
- */
+/*!------------------------------------------------
+@file       MnistReaderCore.h
+@brief      MNISTデータの読み込みで共通する関数を提供する
+@attention  なし
+--------------------------------------------------*/
 
 #ifndef MNISTREADERCORE_H_
 #define MNISTREADERCORE_H_
 
+// インクルードファイル ================================
 #include <string>
 #include <vector>
 #include <iostream>
 #include <fstream>
 
+// クラスの前方宣言 ====================================
+
+// マクロの宣言 ========================================
+
+// 型の宣言 ============================================
+
+// クラス定義 ==========================================
+/*!--------------------------------------------------
+@class      MnistReaderCore.h
+@brief      MNISTデータの読み込みで共通する関数を提供する
+@note       MNISTデータの読み込みの内、データを格納する変数の型に<br>
+              依存しない処理を提供する
+@attention  なし
+@see        なし
+--------------------------------------------------*/
 class MnistReaderCore {
  public:
-  //! Constractor
+  //! Constructor
   MnistReaderCore();
   //! Destructor
   virtual ~MnistReaderCore();
+  //! MNISTデータのラベルデータ読み込み
   std::vector<double> readMnistLabel(const std::string& filename);
-  int reverseInt (int i);
+  //! エンディアン変換
+  int reverseInt (int data);
 
  protected:
  private:
-  // std::vector<double> vecl;
 };
 
 #endif  // MNISTREADERCORE_H_
