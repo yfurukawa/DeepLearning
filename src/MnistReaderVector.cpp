eee9a2c40c241750ec925b39cec0cf3b75b27620
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-05-28 14:22:59 +0900

    readMnistLabelメソッドをone-hot表現に対応させるため、インターフェースを変更

--- src/MnistReaderVector.cpp
+++ src/MnistReaderVector.cpp
@@ -65,9 +65,10 @@ void MnistReaderVector::readMnist(const std::string& filename) {
 @brief      MNISTラベルデータ読み込み
 @note       MnistReaderCoreクラスの同メソッドへ処理を移譲する
 @param[in]  filename  MNISTラベルデータファイル  [-] (-)
+@param[in]  oneHotLabel ラベルをone-hot表現にする  [-] (-)
 @return     MNISTラベルデータが格納されたコンテナ
 @attention  ファイルが存在しない等のエラー処理は未実装
 --------------------------------------------------*/
-std::vector<double> MnistReaderVector::readMnistLabel(const std::string& filename) {
-	return readerCore.readMnistLabel(filename);
+std::vector<double> MnistReaderVector::readMnistLabel(const std::string& filename, const bool& oneHotLabel) {
+	return readerCore.readMnistLabel(filename, oneHotLabel);
 }
