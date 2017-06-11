5cdafce404fe0a2a34ee91d41e45a62622769991
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-06-11 19:14:12 +0900

    設定ファイル読み込み用のFileDAOの追加

--- src/productionObjects.mk
+++ src/productionObjects.mk
@@ -1,1 +1,1 @@
-OBJ = ActivationFunction.o  OutputLayer.o  ThreeLayeredNN.o MnistReaderVector.o MnistReaderOpenCV.o  MnistReaderCore.o ConfigDataHolder.o
+OBJ = ActivationFunction.o  OutputLayer.o  ThreeLayeredNN.o MnistReaderVector.o MnistReaderOpenCV.o  MnistReaderCore.o ConfigDataHolder.o FileDAO.o
