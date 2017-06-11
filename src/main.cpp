0edba5e7480381d62cff6e9b80ccf6b434aec567
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-06-11 21:02:29 +0900

    main関数の中身を別の関数に移動

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,16 @@
 #include <string>
-#include <iostream>
-#include <vector>
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+
 #include "ThreeLayeredNN.h"
-#include "MnistReaderOpenCV.h"
+#include "MnistMain.h"
 
 int main(int argc, char* argv[]) {
+
+		return mnist::mnistMain(argc, argv);
+	/*
 	ThreeLayeredNN network;
 	network.initialize();
 	network.forward();
 
-
-	MnistReaderOpenCV mnistReader;
-	//int number_of_images = 10000;
-	//int image_size = 28 * 28;
-	std::string filenamel = "../dataset/t10k-labels-idx1-ubyte";
-	//read MNIST label into cv::Mat
-	cv::Mat vecl;
-	bool oneHotLabel(true);
-	vecl = mnistReader.readMnistLabel(filenamel, oneHotLabel);
-	std::cout << vecl.size() << std::endl;
-	std::cout << vecl.at<float>(0) << std::endl;
-
-	std::string filename = "../dataset/t10k-images-idx3-ubyte";
-
-    //read MNIST iamge into OpenCV Mat vector
-	std::vector<cv::Mat> vec;
-	//read_Mnist(filename, vec);
-	bool normalize(false);
-	bool flatten(false);
-	vec = mnistReader.readMnist(filename, normalize, flatten);
-	if(vec.size() != 0) {
-		std::cout << vec.size() << std::endl;
-		cv::imshow("1st", vec[0]);
-		cv::waitKey();
-	}
-
-
-
 	return 0;
+	*/
 }
