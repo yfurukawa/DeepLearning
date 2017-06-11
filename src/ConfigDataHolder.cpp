5cdf179caccbcf26461b9cc435301b8243ef1ed3
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2017-06-11 19:10:06 +0900

    設定値を返すメソッドの追加

--- src/ConfigDataHolder.cpp
+++ src/ConfigDataHolder.cpp
@@ -5,13 +5,17 @@
 --------------------------------------------------*/
 
 #include "ConfigDataHolder.h"
+#include "FileDAO.h"
 
 /*!------------------------------------------------
 @brief      デフォルトコンストラクタ
 @note       クラスを構築する
 @attention  なし
 --------------------------------------------------*/
-ConfigDataHolder::ConfigDataHolder() {
+ConfigDataHolder::ConfigDataHolder() : configFileName_("NeuralNetwork.conf"), hiddenLayer_(3), activeFunction_("sigmoid"),
+	inputNode_(784), outputClass_(10), configFile_(NULL)
+{
+	prepareConfigurationData();
 }
 
 /*!------------------------------------------------
@@ -30,3 +34,92 @@ ConfigDataHolder::~ConfigDataHolder() {
 @return     なし
 @attention  なし
 --------------------------------------------------*/
+void ConfigDataHolder::setConfigFileName(const std::string& fileName) {
+	configFileName_ = fileName;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+int ConfigDataHolder::getHiddenLayer() const {
+	return hiddenLayer_;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+std::string ConfigDataHolder::getActiveFunction() const {
+	return activeFunction_;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+int ConfigDataHolder::getInputNode() const {
+	return inputNode_;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+int ConfigDataHolder::getOutputClass() const {
+	return outputClass_;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+std::vector<int> ConfigDataHolder::getHiddenNode() const {
+	  return hiddenNode_;
+}
+
+/*!------------------------------------------------
+@brief
+@note
+@param[in]  パラメータ名  説明  [単位] (範囲)
+@param[out]
+@return     なし
+@attention  なし
+--------------------------------------------------*/
+void ConfigDataHolder::prepareConfigurationData() {
+	// TODO
+	// confファイルの内容を読み込むようにする
+	if(configFile_ == NULL) {
+		configFile_ = new FileDAO();
+	}
+
+	if(configFile_->isExsist(configFileName_)){
+		try {
+			configFile_->openInputter(configFileName_);
+		}
+		catch(const std::ios::failure& e) {
+			std::cout << e.what() << std::endl;
+			exit(1);
+		}
+	}
+}
--- src/ConfigDataHolder.h
+++ src/ConfigDataHolder.h
@@ -8,8 +8,11 @@
 #define CONFIGDATAHOLDER_H_
 
 // インクルードファイル ================================
+#include <string>
+#include <vector>
 
 // クラスの前方宣言 ====================================
+class IInputter;
 
 // マクロの宣言 ========================================
 
@@ -31,13 +34,28 @@ class ConfigDataHolder {
 	  static ConfigDataHolder instance;
 	  return instance;
   }
+  void setConfigFileName(const std::string& fileName);
+  int getHiddenLayer() const;
+  std::string getActiveFunction() const;
+  int getInputNode() const;
+  int getOutputClass() const;
+  std::vector<int> getHiddenNode() const;
 
  protected:
  private:
+  std::string configFileName_;  //!< configファイルのファイル名
+  int hiddenLayer_;  //!< 隠れ層の層数
+  std::string activeFunction_; //!< 隠れ層のアクティブ関数の種類（step : ステップ関数、sigmoid : シグモイド関数）
+  int inputNode_;  //!< 入力層のユニット数
+  int outputClass_;  //!< 出力層のクラス数
+  std::vector<int> hiddenNode_; //!< 隠れ層の出力ノード数
+
+  IInputter* configFile_;
   //! Constructor
   ConfigDataHolder();
   ConfigDataHolder(ConfigDataHolder const&);  // 実装しないこと
   void operator=(ConfigDataHolder const&);    // 実装しないこと
+  void prepareConfigurationData();
 };
 
 #endif  // CONFIGDATAHOLDER_H_
